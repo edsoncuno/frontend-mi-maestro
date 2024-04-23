c56e8e964d3bcc88661c26972a4fe5396b8e6bc6
edsoncuno1@gmail.com <edsoncuno1@gmail.com>
2024-04-23 01:44:13 +0000

    contacto listo

--- src/pages/home/contacto/data.js
+++ src/pages/home/contacto/data.js
@@ -1,9 +1,38 @@
 const data = {
-  whatsapp: { href: "", label: 970555161 },
-  facebook: { href: "", label: "facebook" },
-  telefono: 970555161,
-  direccion: "Av. Rio Perene 711 Sol de Vitarte",
-  correo: "edsoncuno3@gmail.com",
+  lema: "Estamos listos para brindarte soluciones en construcción, reparación y mantenimiento del hogar. ¡Contáctanos hoy para empezar a trabajar en tu proyecto!",
+
+  datos: [
+    {
+      imageUrl: "/icons/whatsapp.svg",
+      href: "",
+      label: "970555161",
+      color: "#27d045",
+    },
+    {
+      imageUrl: "/icons/facebook1.svg",
+      href: "",
+      label: "Mi Maestro",
+      color: "#3c5a9a",
+    },
+    {
+      imageUrl: "/icons/phone.svg",
+      href: "",
+      label: 970555161,
+      color: "#1990a7",
+    },
+    {
+      imageUrl: "/icons/maps.svg",
+      href: "",
+      label: "Av. Rio Perene 711 Sol de Vitarte",
+      color: "#167ee6",
+    },
+    {
+      imageUrl: "/icons/email.svg",
+      href: "",
+      label: "edsoncuno3@gmail.com",
+      color: "#f8b64c",
+    },
+  ],
 };
 
 export default data;
--- src/pages/home/contacto/index.jsx
+++ src/pages/home/contacto/index.jsx
@@ -1,9 +1,46 @@
+"use client";
 import styles from "./index.module.css";
+import data from "./data.js";
 
 export default function App() {
+  const listDatos = data.datos.map((item, index) => {
+    return (
+      <div
+        key={"id-dato-" + index}
+        className={styles.dato}
+        style={{
+          border: `1px solid ${item.color}`,
+        }}
+      >
+        <div
+          className={styles.icon}
+          style={{
+            width: "4rem",
+            height: "4rem",
+            backgroundImage: `url(${item.imageUrl})`,
+            backgroundSize: "80% 80%",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            marginRight: "1rem",
+          }}
+        ></div>
+        <div className={styles.label}>{item.label}</div>
+      </div>
+    );
+  });
+
   return (
     <section id="section-contacto" className={styles.container}>
-      Contacto
+      <h2 className={styles.title}>Contáctanos!</h2>
+      <p className={styles.paragraph}>{data.lema}</p>
+      <iframe
+        className={styles.map}
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d243.8832355482296!2d-76.93598452363076!3d-12.03444829027043!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c41e76af6963%3A0x30e88e869ed2438f!2sQuiquia%20motors!5e0!3m2!1sen!2spe!4v1690312857820!5m2!1sen!2spe"
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+      <div className={styles.datos}>{listDatos}</div>
     </section>
   );
 }
